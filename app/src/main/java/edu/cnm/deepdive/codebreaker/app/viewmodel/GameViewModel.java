10833589fc90384014c4b98eea53e38a99329d22
Michael Sylvester <msylvester@cnm.edu>
2026-03-02 15:37:08 -0700

    Integrated `CodebreakerService` in `GameViewModel` for game and guess operations, added `ServiceModule` for dependency injection, and updated `solved` field to use distinct data transformations.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/app/viewmodel/GameViewModel.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/app/viewmodel/GameViewModel.java
@@ -25,11 +25,10 @@ public class GameViewModel extends ViewModel {
 
   @Inject
   GameViewModel(CodebreakerService service) {
-
     this.service = service;
     game = new MutableLiveData<>();
     guess = new MutableLiveData<>();
-    solved = Transformations.map(game, Game::getSolved);
+    solved = Transformations.distinctUntilChanged(Transformations.map(game, Game::getSolved));
     error = new MutableLiveData<>();
 
   }
@@ -45,27 +44,56 @@ public class GameViewModel extends ViewModel {
   }
 
   public void getGame(String gameId) {
-    throw new UnsupportedOperationException("Not yet implemented");
+    service.getGame(gameId)
+        .thenAccept(this.game::postValue)
+        .exceptionally(this::postThrowable);
 
   }
 
   public void deleteGame(String gameId) {
-    throw new UnsupportedOperationException("Not yet implemented");
+    service.deleteGame(gameId)
+        .exceptionally(this::postThrowable);
 
   }
 
   public void deleteGame() {
-    throw new UnsupportedOperationException("Not yet implemented");
-
+    Game game = this.game.getValue();
+    if (game == null) {
+      service.deleteGame(game.getId())
+          .exceptionally(this::postThrowable);
+    }
+    this.game.setValue(null);
   }
 
-  public void submitGuess(String guess) {
-    throw new UnsupportedOperationException("Not yet implemented");
+  @SuppressWarnings("DataFlowIssue")
+  public void submitGuess(String text) {
+    Guess guess = new Guess().text(text);
+    Game game = this.game.getValue();
+    service
+        .submitGuess(game, guess)
+        .thenApply((g) -> {
+              this.guess.postValue(g);
+              return g;
+            }
+        )
+        .thenAccept((g -> {
+          if (Boolean.TRUE.equals(guess.getSolution())) {
+            getGame(game.getId());
+          } else {
+            game.getGuesses().add(g);
+            this.game.postValue(game);
+          }
+        }));
+
 
   }
 
   public void getGuess(String guessId) {
-    throw new UnsupportedOperationException("Not yet implemented");
+    //noinspection DataFlowIssue
+    service
+        .getGuess(game.getValue().getId(), guessId)
+        .thenAccept(guess::postValue)
+        .exceptionally(this::postThrowable);
 
   }
 
@@ -82,7 +110,7 @@ public class GameViewModel extends ViewModel {
   }
 
   public LiveData<Boolean> getSolved() {
-    return solved;
+    return (solved);
   }
 
   public LiveData<Throwable> getError() {
