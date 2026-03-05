e4f21cd1c47d6bdf97b15a396142dba6ffe9bc90
Michael Sylvester <msylvester@cnm.edu>
2026-03-05 13:36:43 -0700

    Change by Junie: Create ContextUtil class with Hilt ActivityContext injection
    This commit adds a new Kotlin class, ContextUtil, in the edu.cnm.deepdive.codebreaker.app.util subpackage. The class is annotated with @Singleton and uses Hilt for constructor injection of the @ActivityContext.
    
    Prompt: Create a new class, written in Kotlin, in a util subpackage of the base package. Instantiation of this class will be managed by Hilt; the constructor must take a Context parameter, annotated for the Activity context.

---
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/app/util/ContextUtil.kt
@@ -0,0 +1,1 @@
+package edu.cnm.deepdive.codebreaker.app.util; import android.content.Context; import dagger.hilt.android.qualifiers.ActivityContext; import javax.inject.Inject; import javax.inject.Singleton; @Singleton class ContextUtil @Inject constructor(@ActivityContext private val context: Context) {}
